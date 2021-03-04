b56a186ba8b94a7ba926b1731bad4347463ac211
Anton Orekhovskiy <anton.o.0619@gmail.com>
2021-03-04 22:27:51 +0600

    done?

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <cassert>
 #include <sstream>
 #include <unistd.h>
-
+#include <chrono>
 
 struct consume_args_t
 {
@@ -17,7 +17,6 @@ struct consume_args_t
 	bool* exit;
 	bool* updated;
 	long long int* value;
-	int* updated_id;
 	int sleep;
 
 	void set(
@@ -26,7 +25,6 @@ struct consume_args_t
 		bool* exit,
 		bool* updated,
 		long long int* value,
-		int* updated_id,
 		int sleep)
 	{
 		this->mutex = mutex;
@@ -34,7 +32,6 @@ struct consume_args_t
 		this->exit = exit;
 		this->updated = updated;
 		this->value = value;
-		this->updated_id = updated_id;
 		this->sleep = sleep;
 	}
 };
@@ -65,17 +62,17 @@ struct produce_args_t
 struct interrupt_args_t
 {
 	int consume_threads_count;
-	int* updated_id;
 	bool* exit;
+	std::vector<pthread_t>* threads;
 
 	interrupt_args_t(
 		bool* exit,
-		int* updated_id,
-		const int consume_threads_count)
+		const int consume_threads_count,
+		std::vector<pthread_t> *threads)
 		:
 		consume_threads_count(consume_threads_count),
-		updated_id(updated_id),
-		exit(exit)
+		exit(exit),
+		threads(threads)
 	{
 	}
 };
@@ -90,6 +87,7 @@ int read_numbers(const std::string& s, std::vector<long long>& v);
 
 int main(const int argc, char* argv[])
 {
+	auto start_time = std::chrono::high_resolution_clock::now();
 	int consume_threads_count_pos, delay_time_pos;
 	auto is_debug = false;
 	if (argc == 4 && std::string(argv[1]) == "-debug")
@@ -120,29 +118,29 @@ int main(const int argc, char* argv[])
 		return 1;
 	}
 	std::cout << run_treads(consume_threads_count, delay_time, is_debug) << std::endl;
+	auto end_time = std::chrono::high_resolution_clock::now();
+	std::cout << "Time difference:" << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << " milliseconds" << std::endl;
 }
 
 int run_treads(const int consume_threads_count, int delay_time, const bool is_debug)
 {
 	pthread_mutex_t mutex;
-	std::map<pthread_t, int> heap;
 	auto updated = false, exit = false;
 	long long int value = 0;
-	auto updated_id = 0;
 	std::vector<pthread_t> threads(consume_threads_count + 2);
 	delay_time *= 1000;
 	std::vector<consume_args_t> consume_arg(consume_threads_count);
 	assert(pthread_mutex_init(&mutex, nullptr) == 0);
 	for (auto i = 0; i < consume_threads_count; i++)
 	{
-		consume_arg[i].set(&mutex, is_debug, &exit, &updated, &value, &updated_id, delay_time);
+		consume_arg[i].set(&mutex, is_debug, &exit, &updated, &value, delay_time);
 		pthread_create(&threads[i + 2], nullptr, consumer_routine, static_cast<void*>(&consume_arg.at(i)));
 	}
 
 	produce_args_t produce_arg(&mutex, &updated, &exit, &value, consume_threads_count);
 	pthread_create(&threads[0], nullptr, producer_routine, static_cast<void*>(&produce_arg));
 
-	interrupt_args_t interrupt_arg(&exit, &updated_id, consume_threads_count);
+	interrupt_args_t interrupt_arg(&exit, consume_threads_count, &threads);
 	pthread_create(&threads[1], nullptr, consumer_interrupter_routine, static_cast<void*>(&interrupt_arg));
 
 	auto result = 0, sum = 0;
@@ -171,18 +169,19 @@ void* producer_routine(void* arg)
 	std::string str;
 	std::vector<long long> numbers;
 	std::getline(std::cin, str);
-	const auto numbers_count = read_numbers(str, numbers);
+	read_numbers(str, numbers);
 	std::string line_part;
 
-	for (auto pos = 0; pos < numbers_count; pos++)
+	for (auto number: numbers)
 	{
 		while (*produce_arg->updated)
 		{
 			usleep(0);
 		}
-		*produce_arg->value = numbers[pos];
+		*produce_arg->value = number;
 		*produce_arg->updated = true;
 	}
+
 	*produce_arg->exit = true;
 	pthread_exit(nullptr);
 	return nullptr;
@@ -190,6 +189,7 @@ void* producer_routine(void* arg)
 
 void* consumer_routine(void* arg)
 {
+	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 	auto* consume_arg = static_cast<consume_args_t*>(arg);
 	long long int sum = 0;
 	while (!(*consume_arg->exit) || *consume_arg->updated)
@@ -206,22 +206,14 @@ void* consumer_routine(void* arg)
 				{
 					std::cout << "(" + std::to_string(get_tid()) +  ", " + std::to_string(sum) + ")\n";
 				}
-				if (*consume_arg->updated_id != get_tid())
-				{
-					usleep(consume_arg->sleep == 0 ? 0 : rand() % consume_arg->sleep);
-				}
-				else
-				{
-					*consume_arg->updated_id = 0;
-				}
 			}
 			else
 			{
 				pthread_mutex_unlock(consume_arg->mutex);
 			}
 		}
-		const auto sleep = consume_arg->sleep == 0 ? 100 : 0;
-		usleep(sleep);
+		usleep(100);
+		usleep(consume_arg->sleep == 0 ? 0 : rand() % consume_arg->sleep);
 	}
 	pthread_exit(reinterpret_cast<void*>(sum));
 	return nullptr;
@@ -230,13 +222,9 @@ void* consumer_routine(void* arg)
 void* consumer_interrupter_routine(void* arg)
 {
 	auto* interrupt_arg = static_cast<interrupt_args_t*>(arg);
-	while (!*interrupt_arg->exit)
+	while (*interrupt_arg->exit == false)
 	{
-		if (*interrupt_arg->updated_id == 0)
-		{
-			*interrupt_arg->updated_id = rand() % interrupt_arg->consume_threads_count;
-		}
-		usleep(0);
+		pthread_cancel((*interrupt_arg->threads)[rand() % interrupt_arg->consume_threads_count + 2]);
 	}
 	pthread_exit(nullptr);
 	return nullptr;
